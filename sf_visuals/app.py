5894fb9e42f6e4a4959c348341d14002e0d2f9ed
Till Bungert <tillbungert@gmail.com>
2023-05-02 16:46:29 +0200

    feat(app): add tabs

--- sf_visuals/app.py
+++ sf_visuals/app.py
@@ -17,88 +17,101 @@ def main():
     args = parser.parse_args()
     analyser = Analyser(path=args.path)
 
-    app.layout = html.Div(
-        className="container",
+    app.layout = dcc.Tabs(
+        parent_className="container",
+        className="tab-container",
         children=[
-            html.Div(
-                id="sidebar",
-                style={"display": "inline-block"},
+            dcc.Tab(
+                label="Latent Space",
                 children=[
-                    html.H2(
-                        children=f"Experiment folder:",
-                    ),
-                    html.P(f"{args.path}"),
-                    html.H3(
-                        children="Classes:",
-                    ),
-                    dcc.Checklist(
-                        [
-                            {
-                                "label": html.Div(
-                                    [
-                                        f"Class {c}: ",
-                                        dcc.Input(
-                                            id={"type": "class-name", "id": f"{c}"},
-                                            type="text",
-                                            value=f"{c}",
+                    html.Div(
+                        id="sidebar",
+                        style={"display": "inline-block"},
+                        children=[
+                            html.H2(
+                                children=f"Experiment folder:",
+                            ),
+                            html.P(f"{args.path}"),
+                            html.H3(
+                                children="Classes:",
+                            ),
+                            dcc.Checklist(
+                                [
+                                    {
+                                        "label": html.Div(
+                                            [
+                                                f"Class {c}: ",
+                                                dcc.Input(
+                                                    id={
+                                                        "type": "class-name",
+                                                        "id": f"{c}",
+                                                    },
+                                                    type="text",
+                                                    value=f"{c}",
+                                                ),
+                                            ],
+                                            style={"display": "inline-block"},
                                         ),
-                                    ],
-                                    style={"display": "inline-block"},
-                                ),
-                                "value": c,
-                            }
-                            for c in analyser.classes
+                                        "value": c,
+                                    }
+                                    for c in analyser.classes
+                                ],
+                                analyser.classes,
+                                id="checklist-classes",
+                            ),
+                            html.H3(
+                                children="Datasets:",
+                            ),
+                            dcc.RadioItems(
+                                [{"label": "All", "value": "ALL"}]
+                                + [
+                                    {
+                                        "label": html.Div(
+                                            [
+                                                f"Testset {c}",
+                                            ],
+                                            style={"display": "inline-block"},
+                                        ),
+                                        "value": c,
+                                    }
+                                    for c in analyser.testsets
+                                ],
+                                "ALL",
+                                id="checklist-testsets",
+                            ),
+                            html.H3(
+                                children="Color By:",
+                            ),
+                            dcc.RadioItems(
+                                [
+                                    {"label": "Confidence", "value": "confidence"},
+                                    {
+                                        "label": "Source/Target",
+                                        "value": "source-target",
+                                    },
+                                ],
+                                "confidence",
+                                id="checklist-colorby",
+                            ),
                         ],
-                        analyser.classes,
-                        id="checklist-classes",
                     ),
-                    html.H3(
-                        children="Datasets:",
+                    dcc.Graph(
+                        id="latentspace",
+                        className="latentspace",
+                        figure=analyser.plot_latentspace(analyser.testsets[1]),
+                        responsive=True,
+                        clear_on_unhover=True,
                     ),
-                    dcc.RadioItems(
-                        [{"label": "All", "value": "ALL"}]
-                        + [
-                            {
-                                "label": html.Div(
-                                    [
-                                        f"Testset {c}",
-                                    ],
-                                    style={"display": "inline-block"},
-                                ),
-                                "value": c,
-                            }
-                            for c in analyser.testsets
+                    html.Div(
+                        className="hover-preview",
+                        children=[
+                            html.Img(id="curimg", width="512px", height="512px"),
                         ],
-                        "ALL",
-                        id="checklist-testsets",
                     ),
-                    html.H3(
-                        children="Color By:",
-                    ),
-                    dcc.RadioItems(
-                        [
-                            {"label": "Confidence", "value": "confidence"},
-                            {"label": "Source/Target", "value": "source-target"},
-                        ],
-                        "confidence",
-                        id="checklist-colorby",
-                    ),
+                    html.Div(children=[], hidden=True, id="dummy"),
                 ],
             ),
-            dcc.Graph(
-                id="latentspace",
-                className="latentspace",
-                figure=analyser.plot_latentspace(analyser.testsets[1]),
-                responsive=True,
-                clear_on_unhover=True,
-            ),
-            html.Div(
-                className="hover-preview",
-                children=[
-                    html.Img(id="curimg", width="512px", height="512px"),
-                ],
-            ),
-            html.Div(children=[], hidden=True, id="dummy"),
+            dcc.Tab(label="Failures", children=[html.H2("WIP")]),
         ],
     )
 
